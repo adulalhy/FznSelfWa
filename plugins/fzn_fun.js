73ff5e3075e9c6f7b6cdc4045c8a56d366e82657
adulalhy <46670590+adulalhy@users.noreply.github.com>
2021-06-23 21:19:25 +0800

    Update fzn_fun.js

--- plugins/fzn_fun.js
+++ plugins/fzn_fun.js
@@ -1,1 +1,26 @@
-{}
+(async () => {
+			switch(command){
+          case 'asupan':
+                    get_result = await fetchJson(`https://api.lolhuman.xyz/api/asupan?apikey=${LolApi}`)
+                    ini_buffer = await getBuffer(get_result.result)
+                    await fzn.sendMessage(from, ini_buffer, video, { quoted: msg, mimetype: Mimetype.mp4, filename: "asupan.mp4" })
+                    break
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+      }
+  })();		
+//Fun BY Adul Alhy
